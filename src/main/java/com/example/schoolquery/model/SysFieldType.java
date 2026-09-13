e78094381a2b464a84fbd475ba26aa8d26a8d3a0
Timmy <this4u@qq.com>
2026-09-13 16:41:15 +0800

    feat: add logical field type for query metadata

---
+++ src/main/java/com/example/schoolquery/model/SysFieldType.java
@@ -0,0 +1,6 @@
+package com.example.schoolquery.model;
+
+/** Logical field type used by the platform-neutral result metadata contract. */
+public enum SysFieldType {
+    STRING, NUMBER, BOOLEAN, DATE, DATETIME, JSON, UNKNOWN
+}
